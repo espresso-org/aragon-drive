fe3b85946046703e3a3117d3b0f18f844e26a68a
Justin Laroche <justinlaroche95@gmail.com>
2018-06-10 21:36:44 -0400

    Add fileDescriptions

---
+++ app/utils/file-descriptions.js
@@ -0,0 +1,153 @@
+
+export const classNames = {
+    image: 'fa-file-image',
+    pdf: 'fa-file-pdf',
+    word: 'fa-file-word',
+    powerpoint: 'fa-file-powerpoint',
+    excel: 'fa-file-excel',
+    audio: 'fa-file-audio',
+    video: 'fa-file-video',
+    zip: 'fa-file-archive',
+    code: 'fa-file-code',
+    text: 'fa-file-alt',
+    file: 'fa-file'
+}
+
+export const extensions = {
+    gif: {
+        className: classNames.image,
+        description: 'Image'
+    },
+    jpeg: {
+        className: classNames.image,
+        description: 'Image'
+    },
+    jpg: {
+        className: classNames.image,
+        description: 'Image'
+    },
+    png: {
+        className: classNames.image,
+        description: 'Image'
+    },
+    svg: {
+        className: classNames.image,
+        description: 'Vector Image'
+    },
+
+    pdf: {
+        className: classNames.pdf,
+        description: 'PDF document'
+    },
+
+    doc: {
+        className: classNames.word,
+        description: 'MS Word'
+    },
+    docx: {
+        className: classNames.word,
+        description: 'MS Word'
+    },
+
+    ppt: {
+        className: classNames.powerpoint,
+        description: 'MS Powerpoint'
+    },
+    pptx: {
+        className: classNames.powerpoint,
+        description: 'MS Powerpoint'
+    },
+
+    xls: {
+        className: classNames.excel,
+        description: 'MS Excel'
+    },
+    xlsx: {
+        className: classNames.excel,
+        description: 'MS Excel'
+    },
+
+    aac: {
+        className: classNames.audio,
+        description: 'Audio file'
+    },
+    mp3: {
+        className: classNames.audio,
+        description: 'Audio file'
+    },
+    ogg: {
+        className: classNames.audio,
+        description: 'Audio file'
+    },
+
+    avi: {
+        className: classNames.video,
+        description: 'Video'
+    },
+    flv: {
+        className: classNames.video,
+        description: 'Video'
+    },
+    mkv: {
+        className: classNames.video,
+        description: 'Video'
+    },
+    mp4: {
+        className: classNames.video,
+        description: 'Video'
+    },
+
+    gz: {
+        className: classNames.zip,
+        description: 'Archive'
+    },
+    zip: {
+        className: classNames.zip,
+        description: 'Archive'
+    },
+
+    css: {
+        className: classNames.code,
+        description: 'CSS file'
+    },
+    html: {
+        className: classNames.code,
+        description: 'HTML file'
+    },
+    js: {
+        className: classNames.code,
+        description: 'Javascript file'
+    },
+    jsx: {
+        className: classNames.code,
+        description: 'JSX File'
+    },
+    sol: {
+        className: classNames.code,
+        description: 'Solidity'
+    },
+
+    txt: {
+        className: classNames.text,
+        description: 'Text file'
+    },
+
+    file: {
+        className: classNames.file,
+        description: ''
+    }
+}
+
+export const fontAwesomeIcons = [
+    'faFileImage',
+    'faFilePdf',
+    'faFileWord',
+    'faFilePowerpoint',
+    'faFileExcel',
+    'faFileAudio',
+    'faFileVideo',
+    'faFileArchive',
+    'faFileCode',
+    'faFileAlt',
+    'faFile',
+]
