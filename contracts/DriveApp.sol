b69f554682ad6bac2dec181384a9d31106bb1610
Mathew Cormier <mathew.corm@gmail.com>
2018-06-13 21:47:29 -0400

    Remove counter related code

--- contracts/DriveApp.sol
+++ contracts/DriveApp.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.4.4;
 
 import "@aragon/os/contracts/apps/AragonApp.sol";
+import "@aragon/os/contracts/lib/zeppelin/math/SafeMath.sol";
 
 /**
  * Since inheritance is not currently supported (see https://github.com/aragon/aragon-cli/issues/133) 
@@ -151,46 +152,9 @@ contract Datastore {
 contract DriveApp is AragonApp, Datastore {
     using SafeMath for uint256;
 
-    /// Events
-    event Increment(address indexed entity, uint256 step);
-    event Decrement(address indexed entity, uint256 step);
-
-    /// State
-    uint256 public value;
 
 
 
-    /// ACL
-    bytes32 constant public INCREMENT_ROLE = keccak256("INCREMENT_ROLE");
-    bytes32 constant public DECREMENT_ROLE = keccak256("DECREMENT_ROLE");
-    bytes32 constant public DATASTORE_ROLE = keccak256("DATASTORE_ROLE");
-
-
-
-    function initialize() onlyInit external {
-        initialized();
-
-    }
-
-
-
-    /**
-     * @notice Increment the counter by `step`
-     * @param step Amount to increment by
-     */
-    function increment(uint256 step) auth(INCREMENT_ROLE) external {
-        value = value.add(step);
-        Increment(msg.sender, step);
-    }
-
-    /**
-     * @notice Decrement the counter by `step`
-     * @param step Amount to decrement by
-     */
-    function decrement(uint256 step) auth(DECREMENT_ROLE) external {
-        value = value.sub(step);
-        Decrement(msg.sender, step);
-    }
 }
 
 
