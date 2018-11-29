b6ff6039863e2cde6121037d5c259c45f2763e54
Maxime Cote <maxime.cote184@gmail.com>
2018-11-29 15:28:31 -0500

    Fix x icon position on mac

--- app/frontend/src/components/deletable-label.js
+++ app/frontend/src/components/deletable-label.js
@@ -6,7 +6,7 @@ export const DeletableLabel = ({ label, onDeleteClick, ...props }) =>
   <Main {...props} color={`#${label.color}`}>
     {label.name}
     <Delete onClick={() => onDeleteClick && onDeleteClick(label)}>
-      <svg width={10} height={10} style={{ transform: 'scale(0.8)' }}>
+      <svg width={10} height={10} style={{ transform: 'scale(0.8)', verticalAlign: 'middle' }}>
         <path
           d="M10 1.014L6.014 5 10 8.986 8.986 10 5 6.014 1.014 10 0 8.986 3.986 5 0 1.014 1.014 0 5 3.986 8.986 0z"
           fill="#6D777B"
@@ -21,7 +21,6 @@ export const DeletableLabel = ({ label, onDeleteClick, ...props }) =>
 const Main = styled.div(({ color }) => (`
   display: inline-block;
   height: 28px;
-  line-height: 28px;
   font-size: 14px;
   color: #000;  
   background-color: ${color};
@@ -34,17 +33,13 @@ const Main = styled.div(({ color }) => (`
 
 const Delete = styled.div`
   display: inline-block;
-  content: 'x';
   cursor: pointer;
   text-align: center;
-  font-weight: bold;
   width: 20px;
   height: 100%;
-  color: black;
-  font-size: 14px;
   vertical-align: middle;  
   margin-left: 8px;
-  padding-top: 1px;
+  padding-top: 2px;
 `
 
 // text-shadow: 1px 0 0px rgba(0,0,0,0.6), -1px 0 0px rgba(0,0,0,0.6), 0px 1px 0px rgba(0,0,0,0.6), 0px -1px 0px rgba(0,0,0,0.6);
