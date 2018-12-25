839bd279a25fb65f8e3c82dacb31c333f1de4100
Maxime Cote <maxime.cote184@gmail.com>
2018-12-25 14:14:48 -0500

    MainDropdown background

--- app/frontend/src/components/main-drop-down.js
+++ app/frontend/src/components/main-drop-down.js
@@ -1,2 +1,20 @@
 import styled from 'styled-components'
+import React from 'react'
 import { DropDown } from '@aragon/ui'
+
+const arrow = `<svg width='9' height='5' viewBox='0 0 9 5' xmlns='http://www.w3.org/2000/svg'><path d='M0 0h8.36L4.18 4.18z' fill='#ffffff' fill-rule='evenodd'/></svg>`
+
+export const MainDropDown = props =>
+  <Main>
+    <DropDown {...props} />
+  </Main>
+
+const Main = styled.div`
+    display: inline-block;
+    
+    > div > div {
+        color: white;
+        background-image: url("data:image/svg+xml;utf8,${arrow}"), linear-gradient( 130deg,#00B4E6,#00F0E0 );
+        background-position: calc(100% - 15px) 50%, 0 0;
+    }
+`
